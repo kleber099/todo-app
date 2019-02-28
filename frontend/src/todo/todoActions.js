107111a72407f59da3bf79862477979104f41db9
kleber <kleber099@gmail.com>
2019-02-27 22:20:04 -0300

    Finalizando redux do projeto todoApp

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -8,6 +8,13 @@ export const changeDescription = (event) => ({
 })
 
 export const search = () => {
+  return (dispatch, getState) => {
+    const description = getState().todo.description
+    const search = description ? `&description__regex=/${description}/` : ''
+    const request = axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data }))
+  }
+
   const request = axios.get(`${URL}?sort=-createdAt`)
   return {
     type: 'TODO_SEARCHED',
@@ -18,14 +25,30 @@ export const search = () => {
 export const add = (description) => {
   return dispatch => {
     axios.post(URL, { description })
-      .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+      .then(resp => dispatch(clear()))
       .then(resp => dispatch(search()))
   }
-
-
-  const request = axios.post(URL, { description })
-  return [
-    { type: 'TODO_ADDED', payload: request },
-    search()
-  ]
 }
+
+export const markAsDone = (todo) => {
+  return dispatch => {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true})
+      .then(resp => dispatch(search()))
+  }
+}
+
+export const markAsPending = (todo) => {
+  return dispatch => {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false})
+      .then(resp => dispatch(search()))
+  }
+}
+
+export const remove = (todo) => {
+  return dispatch => {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => dispatch(search()))
+  }
+}
+
+export const clear = () => [{ type: 'TODO_CLEAR' }, search()]
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
   constructor(props) {
@@ -17,16 +17,16 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     if (e.key === 'Enter') {
       e.shiftKey ? search() : add(description)
     } else if (e.key === 'Escape') {
-      this.props.handleClear()
+      clear()
     }
   }
 
   render() {
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     return (
       <div role="form" className="todoForm">
         <Grid cols="12 9 10">
@@ -40,9 +40,9 @@ class TodoForm extends Component {
           <IconButton style="primary" icon="plus"
                       onClick={() => add(description)}/>
           <IconButton style='info' icon='search'
-                      onClick={() => search()}/>
+                      onClick={search}/>
           <IconButton style='default' icon='close'
-                      onClick={this.props.handleClear}/>
+                      onClick={clear}/>
         </Grid>
       </div>
     )
@@ -51,6 +51,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ add, changeDescription, search }, dispatch)
+  bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
